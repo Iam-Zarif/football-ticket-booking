6ce37b468e2c5a217291497a4dac4d9736853ac3
Iam-Zarif <zarifraja19@gmail.com>
2026-06-14 17:02:52 +0600

    1,2,3 queries, conflict in number3(shown in comment line)

--- QUERY.sql
+++ QUERY.sql
@@ -57,3 +57,20 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+
+-- Queries
+
+-- 1. Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+select match_id, fixture, base_ticket_price from matches
+where tournament_category ='Champions League' and match_status = 'Available';
+
+-- 2. Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+select user_id, full_name, email from users
+where  full_name like 'Tanvir%' or full_name ilike '%Haque';
+
+-- 3. Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+-- ??? askedd for Retrieve all but in sample output only booking_id, user_id, match_id and payment_status. So I am assuming we need to select only those columns.
+select booking_id, user_id, match_id, coalesce(payment_status, 'Action Required') 
+from bookings 
+where payment_status is null;
